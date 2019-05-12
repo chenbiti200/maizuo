a311953e0bfb5f9c37a420811dc28b590ceb1482
chenbiti200 <449174804@qq.com>
2019-05-12 22:06:20 +0800

    5.12

--- maizuo/modules/me/me.js
+++ maizuo/modules/me/me.js
@@ -1,12 +1,23 @@
 define(["At","jquery","text!me.html"],function(at,$,text){
 		function initView(){
 
-			var data =[
-				["./img/order1.png","电影订单"],
+			var data ={
+			 order:[
+			 	["./img/order1.png","电影订单"],
 				["./img/order2.png","商品订单"]
-			];
+			 ],
+			 content:{
+			 	img :[
+			 		"./img/con1.png","./img/con2.png","./img/con3.png","./img/con4.png"
+			 	],
+			 	con:["卖座网","组合红包","余额","设置"]
+			 }
 
-			var render = at.render(text,{data:data});
+				
+			};
+
+
+			var render = at.render(text,data);
 			$(".Content").html(render);
 
 
